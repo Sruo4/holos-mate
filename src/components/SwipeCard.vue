2f43863ba695552e95faca4f1257f32db06db538
fanhe <hee.fan@outlook.com>
2024-06-17 15:35:52 +0800

    点击卡片响应事件

--- src/components/SwipeCard.vue
+++ src/components/SwipeCard.vue
@@ -1,6 +1,6 @@
 <template>
   <swiper :slidesPerView="'auto'"    :centeredSlides="true"  :spaceBetween="20" :initial-slide="1" class="mySwiper">
-    <swiper-slide v-for="(card, index) in cards" :key="index">
+    <swiper-slide v-for="(card, index) in cards" :key="index" @click="handleCardClick(card)">
       <CardComponent :card="card" />
     </swiper-slide>
   </swiper>
@@ -16,6 +16,20 @@ const cards = [
   { content: '生活习惯', status: '无', icon: ['fas', 'person-walking'], color: '#F2E8CC' },
   { content: '症状记录', status: '无', icon: ['fas', 'heart-circle-check'], color: '#C6CFEA' }
 ];
+
+const handleCardClick = (card) => {
+  switch (card.content) {
+    case '心情状态':
+      console.log('心情状态');
+      break;
+    case '生活习惯':
+      console.log('生活习惯');
+      break;
+    case '症状记录':
+      console.log('症状记录');
+      break;
+  }
+};
 </script>
 
 <style scoped>
